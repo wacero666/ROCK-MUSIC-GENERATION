6b65963728498514397d4b9b09360b3dfceca15d
bhdeng <bhdeng@umich.edu>
2018-12-06 19:42:45 -0500

    finish training

--- gen_midi.py
+++ gen_midi.py
@@ -3,8 +3,9 @@ import dataPreprocessing
 import sys
 
 instr = sys.argv[1]
+filename = sys.argv[2]
 
-gen = np.load('./try_{}.npy'.format(instr.lower()))
+gen = np.load(filename)
 p = dataPreprocessing.matrix_to_midi(gen, instr)
 s = dataPreprocessing.stream.Stream(p)
-s.write('midi', fp='try_{}.midi'.format(instr.lower()))
+s.write('midi', fp='{}.midi'.format(filename.split('.')[0]))
